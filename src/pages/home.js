1f6c073fae446ab48ff9858c394460253d7e5f68
treva556 <marktreva92@gmail.com>
2024-07-12 02:45:18 +0300

    start

--- src/pages/home.js
+++ src/pages/home.js
@@ -17,14 +17,14 @@ function Body() {
       {/* <Nav /> */}
       <header className="text-white py-20">
         <div className="container mx-auto text-center">
-          <h1 className="text-5xl text-black font-bold">Leading the way to a greener future</h1>
+          <h1 className="text-5xl text-black font-bold"> Designing Digital Experiences</h1>
           <p className="mt-4 text-black text-lg">
-            Ecology is the scientific study of the relationships between organisms and their environment, including their physical, chemical.
+          Creating visually appealing, intuitive, and interactive websites that provide exceptional user experiences.
           </p>
-          <div className="mt-8 flex justify-center space-x-4">
-            <button className="bg-blue-700 hover:bg-blue-600 text-white py-2 px-4 rounded">Join Us Now</button>
-            <button className="bg-white text-blue-700 hover:text-blue-600 py-2 px-4 rounded">Get Started</button>
-          </div>
+          {/* <div className="mt-8 flex justify-center space-x-4">
+            <button className=" rounded-2xl bg-blue-700 hover:bg-blue-600 text-white py-2 px-4 ">Join Us Now</button>
+            <button className=" rounded-2xl bg-white text-blue-700 hover:text-blue-600 py-2 px-4 ">Get Started</button>
+          </div> */}
         </div>
       </header>
       <section className=" mb-1 ml-4 mr-4 rounded-3xl color2 py-20">
@@ -51,14 +51,14 @@ function Body() {
             
             <div className=" w-screen bg11 mb-8">
               <h4 className="mt-5 text-2xl font-bold mb-4">Process</h4>
-              <div className="columns-2">
+              <div className="columns-2 text-2xl">
               <div data-aos="fade-up">
               1. Design</div>
               <div data-aos="fade-down">
               2. coding</div>
               <div data-aos="fade-up">
               3. Testing</div>
-              <div data-aos="fade-down">
+              <div className=' mb-9' data-aos="fade-down">
               4. Launch</div>
               </div>
             </div>
@@ -86,23 +86,21 @@ function Body() {
             </li>
             <li>
               <a href="https://farmwise.co.ke/" target="_blank" rel="noopener noreferrer" className="text-blue-500 underline">
-                Farwise
+                Farmwise
               </a>
             </li>
           </ul>
               </div>
         </div>
       </section>
-      <div data-aos="fade-down">
-  Your content here
-</div>
+   
       <section className="color1 text-white py-20">
         <div className="container mx-auto">
           <h3 className="text-3xl font-bold mb-8 text-center">Other Services</h3>
           <div className="flex flex-wrap justify-center space-x-4">
             <div className="border border-white p-4 m-4 w-1/3">
               <h4 className="text-2xl font-bold mb-4">Multi-Media</h4>
-              <p>This allows individuals and organizations to support their efforts to combat carbon climate change.</p>
+              <p className=' text-xs'>This allows individuals and organizations to support their efforts to combat carbon climate change.</p>
             </div>
             <div className="border border-white p-4 m-4 w-1/3">
               <h4 className="text-2xl font-bold mb-4">Penetration Tester</h4>
@@ -115,6 +113,8 @@ function Body() {
           </div>
         </div>
       </section>
+      <div data-aos="fade-down">
+
       <div className="py-20">
         <h6 className="text-2xl font-bold">Hobbies</h6>
         <ul className="list-disc list-inside">
@@ -122,6 +122,7 @@ function Body() {
           <li>A whole lot of digital love for less</li>
           <li>Problem solving</li>
         </ul>
+      </div>
       </div>
     </div>
   );
