c8992edbf668dcb0c7553351ab06d060fd5b2d5c
treva556 <marktreva92@gmail.com>
2024-07-21 22:25:23 +0300

    mile

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 function Nav() {
   return (
-    <div className=" bg-yellow-400">
+    <div className=" bg-black">
      <nav class=" border-gray-200 ">
-  <div class=" relative max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-2">
+  <div class=" relative max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
     {/* <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
         <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
         <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
@@ -18,20 +18,20 @@ function Nav() {
     <div class="hidden w-full md:block md:w-auto" id="navbar-default">
     <ul className=" right-0 font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg  md:flex-row md:space-x-8 md:ml-auto rtl:space-x-reverse md:mt-0 md:border-0  ">
     <li>
-          <a href="/" class="block py-2 px-3 text-black bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
+          <a href="/" class="block py-2 px-3 text-yellow-400 bg-yellow-400 rounded md:bg-transparent md:p-0 dark:text-yellow " aria-current="page">Home</a>
         </li>
         <li>
-          <a href="#" class="absolute right-60 block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-black md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
+          <a href="#" class="absolute right-60 block py-2 px-3 text-yellow-400 rounded hover:bg-gray-100 md:hov700er:bg-transparent md:border-0 md:p-0 dark:text-yellow-400  dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
         </li>
         <li>
-          <a href="/services" class="absolute right-40 block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
+          <a href="/services" class="absolute right-40 block py-2 px-3 text-yellow-400 roundedrounded hover:bg-gray-100 md:hover:bg-transparent md:border-0  md:p-0 dark:text-yellow-300 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
         </li>
         <li>
-        <a href="#" class="absolute right-20 block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-black md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
+        <a href="#" class="absolute right-20 block py-2 px-3 text-yellow-400 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0  md:p-0 dark:text-yellow-300 dark:hover:bg-gray-700 dark:hover:text-yellow-400 md:dark:hover:bg-transparent">Pricing</a>
 
         </li>
         <li>
-        <a href="#" class="absolute right-0 block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-black md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
+        <a href="#" class="absolute right-0 block py-2 px-3 text-yellow-400 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0  md:p-0 dark:text-yellow-300 dark:hover:bg-gray-700 dark:hover:text-yellow-300 md:dark:hover:bg-transparent">About</a>
 
         </li>
       </ul>
