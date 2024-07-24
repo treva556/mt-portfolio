085f23ec158ab6b6a3321acf2d47c0fbc08cee3f
treva556 <marktreva92@gmail.com>
2024-07-24 18:07:49 +0300

    mile

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -36,9 +36,9 @@ function Nav() {
                 <li>
                   <a href="/" className="block py-2 px-3 text-yellow-400 bg-yellow-400 rounded md:bg-transparent md:p-0 dark:text-yellow" aria-current="page">Home</a>
                 </li>
-                <li>
+                {/* <li>
                   <a href="#" className="block py-2 px-3 text-yellow-400 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 dark:text-yellow-300 dark:hover:bg-gray-700 dark:hover:text-yellow-400 md:dark:hover:bg-transparent">Pricing</a>
-                </li>
+                </li> */}
                 <li>
                   <a href="/blogs" className="block py-2 px-3 text-yellow-400 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 dark:text-yellow-300 dark:hover:bg-gray-700 dark:hover:text-yellow-400 md:dark:hover:bg-transparent">Blogs</a>
                 </li>
